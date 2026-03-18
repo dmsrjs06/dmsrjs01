6d32dcf3080c4066efa155ebdfa8b406f90eb6dc
dmsrjs06 <kimeungun0606@gmail.com>
2026-03-18 12:05:43 +0900

    Update fx2.2.c

--- fx2.2.c
+++ fx2.2.c
@@ -2,11 +2,19 @@
 #define MAX 100
 
 int main() {
-
-	char str[MAX];
-
-	fgets(str, sizeof(str), stdin);
-
+    char str[MAX];
+    int i = 0;
+    int ch;
+	
+    while (i < MAX - 1) {
+        ch = getchar();
+        
+        if (ch == EOF || ch == '\n')
+            break;
+            
+        str[i++] = (char)ch;
+    }
+	
 	for (int i = 0; str[i] != '\0'; i++) {
 		if (str[i] >= 'A' && str[i] <= 'Z')
 			str[i] = str[i] + 32;
